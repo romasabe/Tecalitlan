7996f933fdf92a706e87405e01e736e0dda1f149
romasabe <147640778+romasabe@users.noreply.github.com>
2024-06-01 14:59:23 -0500

    changed an image

--- menu.js
+++ menu.js
@@ -81,6 +81,7 @@ function order(section) {
     ordered.push(section.menuItem);
     prices.push(section.price);
     priceCount();
+    alert(count(ordered))
 };
 
 // remove an item from the order if it has been ordered
